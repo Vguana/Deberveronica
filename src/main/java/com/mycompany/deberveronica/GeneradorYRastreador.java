1197ff35689708fa456584fbb4ad3dc4ee43d44d
VERONICA <Vguana8891@utm.edu.ec>
2023-10-29 22:18:04 -0500

    Commit 3

--- src/main/java/com/mycompany/deberveronica/GeneradorYRastreador.java
+++ src/main/java/com/mycompany/deberveronica/GeneradorYRastreador.java
@@ -1,3 +1,18 @@
 // GeneradorYRastreador.java
+import java.util.Random;
+
 public class GeneradorYRastreador {
+    Random random = new Random();
+    int nivel;
+    int tipo;
+
+    public GeneradorYRastreador(int nivel, int tipo) {
+        this.nivel = nivel;
+        this.tipo = tipo;
+    }
+
+    public int[] generarOperandos() {
+        int limite = (int) Math.pow(10, nivel);
+        return new int[]{random.nextInt(limite), random.nextInt(limite)};
+    }
 }
