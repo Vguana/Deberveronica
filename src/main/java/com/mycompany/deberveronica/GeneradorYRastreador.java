ed200d1e4c5d965d20ba0b4c6e4d75afb9db910d
VERONICA <Vguana8891@utm.edu.ec>
2023-10-29 22:21:24 -0500

    Commit 5

--- src/main/java/com/mycompany/deberveronica/GeneradorYRastreador.java
+++ src/main/java/com/mycompany/deberveronica/GeneradorYRastreador.java
@@ -25,4 +25,14 @@ public void respuestaCorrecta() {
 public void respuestaIncorrecta() {
     incorrectas++;
 }
+
+public void generarMensajePositivo() {
+    String[] mensajes = {"¡Muy bien!", "¡Excelente!", "¡Buen trabajo!", "¡Sigue así!"};
+    System.out.println(mensajes[new Random().nextInt(mensajes.length)]);
+}
+
+public void generarMensajeNegativo() {
+    String[] mensajes = {"No. Por favor intenta de nuevo.", "Incorrecto. Intenta una vez más.", "¡No te rindas!", "No. Sigue intentando."};
+    System.out.println(mensajes[new Random().nextInt(mensajes.length)]);
+}
 }
