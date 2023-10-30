a25e41007401d753ea3f7b0484fa4945ce61aad3
VERONICA <Vguana8891@utm.edu.ec>
2023-10-29 22:17:22 -0500

    Commit 2

--- src/main/java/com/mycompany/deberveronica/ClasePrincipal.java
+++ src/main/java/com/mycompany/deberveronica/ClasePrincipal.java
@@ -1,5 +1,8 @@
 // ClasePrincipal.java
+import java.util.Scanner;
+
 public class ClasePrincipal {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
     }
 }
